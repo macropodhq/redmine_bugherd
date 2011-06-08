dfe5d57e6d645e1812412ad051f2ef4792f12f38
Vincent Brendel <vbrendel@vb.local>
2011-06-08 15:49:03 +1000

    assignee

--- app/controllers/bugherd_controller.rb
+++ app/controllers/bugherd_controller.rb
@@ -34,6 +34,7 @@ class BugherdController < ApplicationController
     @issue.subject = params[:description] if params[:description]
     @issue.status = best_match_status(params[:status_id].to_i) if params[:status_id]
     @issue.priority = best_match_priority(params[:priority_id].to_i) if params[:priority_id]
+    @issue.assignee = User.find_by_mail(params[:assignee]) if params[:assignee]
     
     if @issue.save
       render :text => "OK #{@issue.id}"
