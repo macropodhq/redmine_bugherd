858ab1fd5e5402b4f31ed1837dc9302d95123e70
Vincent Brendel <vbrendel@vb.local>
2011-07-27 16:43:47 +1000

    Web hook trigger

--- app/controllers/bugherd_controller.rb
+++ app/controllers/bugherd_controller.rb
@@ -1,6 +1,22 @@
 class BugherdController < ApplicationController
   unloadable
-  accept_key_auth :update, :add_comment, :project_list, :status_list, :priority_list
+  accept_key_auth :update, :add_comment, :project_list, :status_list, :priority_list, :trigger_web_hook
+
+  def trigger_web_hook
+    @project = Project.find(params[:project_id])
+    
+    field = ProjectCustomField.find_by_name('BugHerd Project Key')
+    return unless field
+
+    value = CustomValue.find_by_customized_id_and_custom_field_id(issue.project.id, field.id)
+    return unless value
+
+    project_key = value.value
+    return unless project_key.present?
+
+    http = Net::HTTP.new(BUGHERD_URL, BUGHERD_PORT)
+    http.get("/redmine_web_hook/#{project_key}")
+  end
 
   def project_list
     list = []
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ ActionController::Routing::Routes.draw do |map|
   map.connect 'bugherd/project_list.:format', :controller => 'bugherd', :action => 'project_list'
   map.connect 'bugherd/status_list.:format', :controller => 'bugherd', :action => 'status_list'
   map.connect 'bugherd/priority_list.:format', :controller => 'bugherd', :action => 'priority_list'
+  map.connect 'bugherd/trigger_web_hook/:project_id.:format', :controller => 'bugherd', :action => 'trigger_web_hook'
   map.connect 'bugherd/issue/update.:format', :controller => 'bugherd', :action => 'update'
   map.connect 'bugherd/issue/add_comment.:format', :controller => 'bugherd', :action => 'add_comment'
 end
